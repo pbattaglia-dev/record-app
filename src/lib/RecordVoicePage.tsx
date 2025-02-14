e48dc7793a0e440e0ae285374dcaf938b41fd78c
Pierre Battaglia <pbattaglia@hopper.com>
2025-02-14 11:13:35 +1000

    add play, pause, stop feature

--- src/lib/RecordVoicePage.tsx
+++ src/lib/RecordVoicePage.tsx
@@ -1,40 +1,72 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useCallback, useEffect } from 'react';
 import { Box, Button, Typography } from '@mui/material';
 import styles from "./RecordVoicePage.module.scss";
 import { ReactComponent as Pause } from "./pause.svg";
 import { ReactComponent as Play } from "./play.svg";
+import { ReactComponent as Stop } from "./stop.svg";
 
 export const RecordVoicePage = () => {
   const recognitionRef = useRef<SpeechRecognition>(null);
 
   const [isActive, setIsActive] = useState<boolean>(false);
-  const [text, setText] = useState<string>();
+  const [isPause, setIsPause] = useState<boolean>(false);
+  const [text, setText] = useState<string>('');
 
-  function handleOnRecord() {
-    if ( isActive ) {
-      recognitionRef.current?.stop();
-      setIsActive(false);
-      return;
+  const isActiveRef = useRef<boolean>(null);
+  const isPauseRef = useRef<boolean>(null);
+  const speechRef = useRef<string>('');
+
+  useEffect(() => {
+    isActiveRef.current = isActive;
+    isPauseRef.current = isPause;
+    speechRef.current = text;
+  }, [isActive, isPause, text]);
+
+  const initializeRecognition = useCallback(() => {
+    if (!isActive) {
+      const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+      recognitionRef.current = new SpeechRecognition();
+
+      recognitionRef.current.onstart = () => {
+        setIsActive(true);
+      }
+
+      recognitionRef.current.onend = () => {
+        if (isActiveRef.current && !isPauseRef.current) {
+          recognitionRef.current?.start();
+        }
+      }
+
+      recognitionRef.current.onresult = (event) => {
+        const transcript = event.results[0][0].transcript;
+        setText(speechRef.current.concat(', ', transcript));
+      }
     }
 
-    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    recognitionRef.current = new SpeechRecognition();
+    recognitionRef.current?.start();
+  }, [ isActive ]);
 
-    recognitionRef.current.onstart = function() {
-      setIsActive(true);
+  const handleOnRecord = useCallback(() => {
+    if (isPause) {
+      setIsPause(false);
     }
 
-    recognitionRef.current.onend = function() {
-      setIsActive(false);
-    }
+    initializeRecognition();
+  }, [ isPause, initializeRecognition ]);
 
-    recognitionRef.current.onresult = async function(event) {
-      const transcript = event.results[0][0].transcript;
-      setText(transcript);
-    }
+  const handleOnPause = useCallback(() => {
+    recognitionRef.current?.stop();
+    setIsPause(true);
+    setText(text + '\n');
+  }, [ text ]);
 
-    recognitionRef.current.start();
-  }
+  const handleOnStop = useCallback(() => {
+    recognitionRef.current?.abort();
+    recognitionRef.current = null;
+    setIsActive(false);
+    setIsPause(false);
+    setText('')
+  }, []);
 
   return (
     <Box
@@ -85,9 +117,9 @@ export const RecordVoicePage = () => {
             padding: "12px",
             backgroundColor: "blue",
           }}
-          onClick={handleOnRecord}
+          onClick={!isActive || isPause ? handleOnRecord : handleOnPause}
         >
-          {isActive ? (
+          {isActive && !isPause ? (
             <Pause className={styles.icon} />
           ) : (
             <Play className={styles.icon} />
@@ -100,8 +132,14 @@ export const RecordVoicePage = () => {
             height: "100%",
             borderRadius: "20px",
             backgroundColor: "red",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "12px",
           }}
+          onClick={handleOnStop}
         >
+          <Stop className={styles.icon} />
         </Button>
       </Box>
     </Box>
